f0e3025d160663ae6cca5796982621fd45685bd2
oloyedev <oloyeadz@gmail.com>
2024-12-23 21:14:29 -0800

    Your commit message

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ import os
 app = Flask(__name__)
 from flask_cors import CORS
 
-from flask_cors import CORS
-
-CORS(app, resources={r"/*": {"origins": "https://bananart.xyz"}})
-
+CORS(app, resources={r"/*": {"origins": ["https://bananart.xyz", "https://api.bananart.xyz"]}})
 
  # Update to the specific domain for security
 
